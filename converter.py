8ca1b6381d8b961fb319c3bfb0d07d3b954da640
Luke Hollis <lukehollis@gmail.com>
2017-02-18 21:25:02 -0500

    update converter to flatten nodes with only one child and regenerate json files

--- converter.py
+++ converter.py
@@ -2,6 +2,7 @@ import os, re
 import json
 import pdb
 import collections
+from django.utils.text import slugify
 from bs4 import BeautifulSoup
 
 sourceLink = 'http://gretil.sub.uni-goettingen.de/#OldJavanese'
@@ -10,10 +11,13 @@ works = []
 
 def jaggedListToDict(text):
 	node = { str(i): t for i, t in enumerate(text) }
-	node = collections.OrderedDict(sorted(node.items()))
+	node = collections.OrderedDict(sorted(node.items(), key=lambda k: int(k[0])))
 	for child in node:
 		if isinstance(node[child], list):
-			node[child] = walkText(node[child])
+			if len(node[child]) == 1:
+				node[child] = node[child][0]
+			else:
+				node[child] = jaggedListToDict(node[child])
 	return node
 
 def main():
@@ -45,7 +49,7 @@ def main():
 				text = soup.body.text.split('\n')
 				text = [node.strip() for node in text if len(node.strip())]
 				work['text'] = jaggedListToDict(text)
-				fname = work['source'] + '__' + work['englishTitle'] + '__' + work['language'] + '.json'
+				fname = slugify(work['source']) + '__' + slugify(work['englishTitle'][0:140]) + '__' + slugify(work['language']) + '.json'
 				fname = fname.replace(" ", "")
 				with open('cltk_json/' + fname, 'w') as f:
 					json.dump(work, f)
